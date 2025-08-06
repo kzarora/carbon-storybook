61a397ce9ba1a48aac916a835233094b4eba52a9
Kshitiz Arora <kshitiza@adobe.com>
2025-08-06 12:51:36 +0530

    Initial commit of my design system dreams

---
+++ src/components/FancyButton.stories.tsx
@@ -0,0 +1,12 @@
+import { FancyButton } from "./FancyButton";
+
+export default {
+  title: "IBM Carbon/FancyButton",
+  component: FancyButton,
+};
+
+export const Default = {
+  args: {
+    label: "Click me, corporate edition",
+  },
+};
